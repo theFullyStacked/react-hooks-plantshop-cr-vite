d43b9dcce3b342a29dbd96714c6971f5d74c67f7
William Munene <williammunene2018@gmail.com>
2026-06-06 23:17:52 +0300

    working build

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage
+        plants={plants}
+        onAddPlant={handleAddPlant}
+      />
     </div>
   );
 }
--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantPage({ plants, onAddPlant }) {
+  const [searchText, setSearchText] = useState("");
+
+  function handleSearchChange(text) {
+    setSearchText(text);
+  }
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={onAddPlant} />
+      <Search searchText={searchText} onSearchChange={handleSearchChange} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
